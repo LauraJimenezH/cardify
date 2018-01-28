49af684136966812e9a92ba251b07aea5f3f72fc
MarinaRH <l_marina1717@hotmail.com>
2018-01-27 22:07:56 -0500

    Modificando estructura

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,25 @@
-document.addEventListener('load',() => {
 
-  function pluginHover() {
+window.addEventListener('load',() => {
+    const container = document.getElementById('container-images');
+    const images = document.getElementsByTagName('img');
 
-    var container = document.getElementsByClassName('container-images');
-    var images = document.getElementsByClassName('hover-img');
+    function pluginHover() {
+      for (var i = 0; i< images.length; i++) {
+        images[i].addEventListener('click', () => {
+          // container.removeChild(event.target);
+        let figure=document.createElement('figure');
+         event.target.appendChild(figure);
 
-    images.addEventListener('mouseover', () => {
-      images.innerHTML = `<figure><img src=` +  + `</figure>`
-    })
-
-  }
-
+        let figurecaption=document.createElement('p');
+        figure.appendChild(figurecaption);
+        figurecaption.innerHTML=event.target.alt;
+        
+        // var img=document.createElement('img');
+        // figure.appendChild(img);
+        // img.setAttribute('src', this.src);
+        // this.replaceChild(figure);
+        });
+      }
+    }
+    pluginHover();
 });
