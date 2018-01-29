7e743a75f4cadea13be05283f120995edf4426c4
MarinaRH <l_marina1717@hotmail.com>
2018-01-29 14:02:07 -0500

    modificando estructura

---
+++ src/index.js
@@ -0,0 +1,25 @@
+
+window.addEventListener('load', () => {
+    const container = document.getElementById('container-images');
+    const images = document.getElementsByTagName('img');
+
+    function pluginHover() {
+      for (var i = 0; i< images.length; i++) {
+        images[i].addEventListener('click', () => {
+          // container.removeChild(event.target);
+        let figure = document.createElement('figure');
+         event.target.appendChild(figure);
+
+        let figurecaption = document.createElement('p');
+        figure.appendChild(figurecaption);
+        figurecaption.innerHTML = event.target.alt;
+        
+        // var img=document.createElement('img');
+        // figure.appendChild(img);
+        // img.setAttribute('src', this.src);
+        // this.replaceChild(figure);
+        });
+      }
+    }
+    pluginHover();
+});
