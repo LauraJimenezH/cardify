a823272e12e141ea931913b649d6949c6953e974
LauraJH16 <laurajimenezh16@hotmail.com>
2018-01-31 11:18:08 -0500

    Agregando estilos

--- public/js/index.js
+++ public/js/index.js
@@ -33,7 +33,7 @@
     $(this).find('img').each(function () {
       $(this).wrap('<figure></figure>');
       $(this).hover(function () {
-        $(this).parent().append($('<figcaption>' + $(this).attr('alt') + '</figcaption>'));
+        $(this).parent().append($('<figcaption class="figcaption-hover">' + $(this).attr('alt') + '</figcaption>'));
       }, function () {
         $('figcaption').remove();
       });
