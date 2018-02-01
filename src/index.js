1ea3b1c6ba8f5b2eb1199e59e83a30be3d5d666c
MarinaRH <l_marina1717@hotmail.com>
2018-02-01 17:09:59 -0500

    Modificando estilos de index

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
   $.fn.cardify = function () {
     $(this).find('img').each(function() {
       $(this).wrap('<figure></figure>');
-    $(this).mouseover(() => {
+    $(this).hover(() => {
       $(this).parent().append($('<figcaption class="figcaption-hover">' + $(this).attr('alt') + '</figcaption>'));      
         },()=>{$('figcaption').remove();});
       });
