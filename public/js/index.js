3b8ad0a093d3197b6e4a98be8a45d910d3122c12
LauraJH16 <laurajimenezh16@hotmail.com>
2018-02-05 00:30:19 -0500

    Modificando identacion

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
-(function ($) {
-  $.fn.cardify = function () {
-    $(this).find('img').each(function () {
+(function($) {
+  $.fn.cardify = function() {
+    $(this).find('img').each(function() {
       var _this = this;
 
       $(this).wrap('<figure class="snip1328"></figure>');
-      $(this).hover(function () {
+      $(this).hover(function() {
         $(_this).parent().append($('<figcaption class="figcaption-hover">' + $(_this).attr('alt') + '</figcaption>'));
-      }, function () {
+      }, function() {
         $('figcaption').remove();
       });
     });
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-
 (($) => {
-  $.fn.cardify = function () {
+  $.fn.cardify = function() {
     $(this).find('img').each(function() {
       $(this).wrap('<figure></figure>');
-    $(this).hover(() => {
-      $(this).parent().append($('<figcaption class="figcaption-hover">' + $(this).attr('alt') + '</figcaption>'));      
-      },()=>{$('figcaption').remove();});
+      $(this).hover(() => {
+        $(this).parent().append($('<figcaption class="figcaption-hover">' + $(this).attr('alt') + '</figcaption>'));      
+      }, ()=>{
+$('figcaption').remove();
+});
     });
   };
 })(jQuery);
