4b7a126279b6472542928ee974dc618ff5d7a0a5
LauraJH16 <laurajimenezh16@hotmail.com>
2018-02-04 22:27:10 -0500

    Añadiendo estilos a demo

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@
     $(this).find('img').each(function () {
       var _this = this;
 
-      $(this).wrap('<figure></figure>');
+      $(this).wrap('<figure class="snip1328"></figure>');
       $(this).hover(function () {
         $(_this).parent().append($('<figcaption class="figcaption-hover">' + $(_this).attr('alt') + '</figcaption>'));
       }, function () {
